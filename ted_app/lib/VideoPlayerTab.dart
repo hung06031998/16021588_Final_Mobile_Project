8009fba93332d5ee0d7d29b639e51411d365fca8
HungSourcetree <hungnv6398@gmail.com>
2020-07-05 16:48:08 +0700

    implement InfoTab

--- ted_app/lib/VideoPlayerTab.dart
+++ ted_app/lib/VideoPlayerTab.dart
@@ -46,11 +46,67 @@ class _VideoPlayerTabState extends State<VideoPlayerTab>{
         title: Text(data['name']),
         backgroundColor: Colors.red,
       ),
-      body: Center(
-        child: YoutubePlayer(
-          controller: _controllerYoutube,
-
+      body: ListView(
+        children: <Widget>[
+          YoutubePlayer(controller: _controllerYoutube,),
+          Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: ListTile(
+                title: Text(
+                  "Watch next",
+                  style: Theme.of(context).textTheme.title,
+                ),
+              )),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: Image.network(
+            "https://img.youtube.com/vi/RQW3zC5QaY4/maxresdefault.jpg",
+            fit: BoxFit.fill,
+          ),
+          title: Text("What do all languages have in common?"),
+          subtitle: Text(
+            "Cameron Morin",
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: null,
+          ),
         ),
+      ),Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: Image.network(
+            "https://img.youtube.com/vi/NVd8kuufBhM/maxresdefault.jpg",
+            fit: BoxFit.fill,
+          ),
+          title: Text("What really happened during the Salem Witch Trials"),
+          subtitle: Text(
+            "Brian A. Pavlac",
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: null,
+          ),
+        ),
+      ),Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: Image.network(
+            "https://img.youtube.com/vi/mmoFxVqZ9z4/maxresdefault.jpg",
+            fit: BoxFit.fill,
+          ),
+          title: Text("Why should you read “Moby Dick”?"),
+          subtitle: Text(
+            "Sascha Morrell",
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: null,
+          ),
+        ),
+      ),
+        ],
       ),
     );
   }
