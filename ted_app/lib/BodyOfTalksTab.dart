a7713b8ffebd9d1e52a9ac808a508384817c5888
HungSourcetree <hungnv6398@gmail.com>
2020-07-05 12:29:59 +0700

    implement body of TalkTab and InfoTab

--- ted_app/lib/BodyOfTalksTab.dart
+++ ted_app/lib/BodyOfTalksTab.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:tedapp/InfoTab.dart';
 
 class BodyOfTalksTab extends StatefulWidget {
   String state;
@@ -36,51 +37,60 @@ class _BodyOfTalksTabState extends State<BodyOfTalksTab> {
             return StaggeredTile.count(index == 0 ? 4 : 2, index == 0 ? 2 : 1);
           },
           itemBuilder: (context, index) {
-            return Card(
-              child: Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(snapshot.data.documents[index]["urlImage"]),
-                        fit: BoxFit.cover,
-                        alignment: Alignment.center
-                    )
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 4,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(6.0, 6.0, 0.0, 6.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(snapshot.data.documents[index]["speakers"], textAlign: TextAlign.left, style: TextStyle(color: Colors.white)),
-                            Text(snapshot.data.documents[index]["name"], textAlign: TextAlign.left, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-                          ],
+            return InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => InfoTab(snapshot.data.documents[index])));
+              },
+              child: Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(snapshot.data.documents[index]["urlImage"]),
+                          fit: BoxFit.cover,
+                          alignment: Alignment.center
+                      )
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(6.0, 6.0, 0.0, 6.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                color: Colors.black12,
+                                  child: Text(snapshot.data.documents[index]["speakers"], textAlign: TextAlign.left, style: TextStyle(color: Colors.white))),
+                              Container(
+                                color: Colors.black12,
+                                  child: Text(snapshot.data.documents[index]["name"], textAlign: TextAlign.left, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-                          GestureDetector(
-                            child: Icon(Icons.more_vert, color: Colors.white,),
-                            onTap: (){},
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 6.0, right: 6.0, top: 6.0),
-                            child: Text(snapshot.data.documents[index]["duration"], style: TextStyle(color: Colors.white),),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            GestureDetector(
+                              child: Icon(Icons.more_vert, color: Colors.white,),
+                              onTap: (){},
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 6.0, right: 6.0, top: 6.0),
+                              child: Text(snapshot.data.documents[index]["duration"], style: TextStyle(color: Colors.white),),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             );
